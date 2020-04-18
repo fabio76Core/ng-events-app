7483fe15e5c10092dfc830363d5f4aca37141d08
unknown <fabio76core@gmail.com>
2020-04-18 17:45:31 +0200

    first commit

---
+++ src/app/user/profile.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
+import { FormControl, FormGroup } from '@angular/forms'
+import { AuthService } from './auth.service'
+
+@Component({
+  templateUrl: './profile.component.html'
+})
+export class ProfileComponent implements OnInit {
+  profileForm:FormGroup
+
+  constructor(private authService: AuthService, private router:Router){
+
+  }
+  ngOnInit(){
+    let firstName = new FormControl(this.authService.currentUser.firstName);
+    let lastName = new FormControl(this.authService.currentUser.lastName);
+
+    this.profileForm = new FormGroup({
+      firstName: firstName,
+      lastName: lastName
+    })
+  }
+
+  saveProfile(formValues){
+    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
+    this.router.navigate(['events']);
+  }
+
+  cancel(){
+    this.router.navigate(['events']);
+  }
+}
